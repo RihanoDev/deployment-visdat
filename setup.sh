07ec6b749cc1a6c13f45938f20383e458eaf2edb
Rihano <74526376+RihanoDev@users.noreply.github.com>
2022-01-05 23:50:40 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,6 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"rhr2109@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "\[server]
+headless = true
+enableCORS=false
+port = $PORT
 " > ~/.streamlit/config.toml
